fd20dc0505b1b2ef419d0f7703845ac6326497bb
AsociTon <asociton@outlook.com>
2020-05-22 16:45:28 +0530

    Add notes regarding other concepts.
    * Props in vue
    * Primitive types and non primitive types
    * Events in vue
    * The event bus
    * Lifecycle hooks
    * Slots
    * Dynamic Components
    * Input Binding

--- vue-res/app.js
+++ vue-res/app.js
@@ -270,3 +270,130 @@ methods: {
 // this by adding a ex. h1[as35-afe3yt4n] kinda identifier to all the elements of that particular 
 // component making sure, all the style properties mentioned are applied to the designated elements
 // of that component.
+
+// Props in vue
+//
+// Similar to react in vue too we can pass props from parent to child. We will need a reciever in
+// the child for the incoming props. To set up a receiver for props:
+// props : ['name_of_incoming_prop'] => this can be then used as any other data present in 
+//          the component
+//
+// Validators in props
+// a better way to recieve props is to validate what we are recieving, we can add a validator for
+// props in vue
+// props: {
+//     name_of_incoming_prop: { type: Array, required: true }   
+// }
+// Thus we will reciever an error in the console in case of any other data than of type array.
+//
+// To send props we simply add an attribute by the name in the component tag included in the
+// parents dom, by default the attribute will only pass the string provided to the attribute as
+// hardcoded their, but to use that string to indicate a variable of the parent, we will need to
+// bind the attribute using v-bind:
+//ex.
+// <child-component-name name_of_prop_to_send="variable_name" ></child-component-name> => x
+// this will simply send the string "variable_name" to the child, so we v-bind the attribute
+// <child-component-name v-bind:name_of_prop_to_send="variable_name" ></child-component-name>
+
+
+// Pimitive types and Reference types
+//
+// The non primitive data types are passed as a reference to the original data via props resulting
+// into updation in all the places wherever that data is used.
+//
+// In case of primitive data type ex. string, we pass a copy of the data thus the change in data is
+// local to that particular component only.
+
+
+// Events in vue ( Creating our own custom events from child to parent )
+//
+// One use case could be when we want to update a primitive data in all the places where it is being
+// used.
+//
+// The procedure is create $emit('event-name', data_you_wanna_send_with_the_event);
+// just execute the above line and an event would be emiited all the way to the parent.
+//
+// To revcieve/catch the event add an event listener in the child-component tag which is included
+// in the parent. ex.
+//
+//  <child-from-which-we-are-expecting-signal v-on:event-name="do_this($event)">
+//  </child-from-which-we-are-expecting-signal>
+//
+// In the above example we see use of v-on to listen for our custom event event-name, we use the 
+// do_this method declared in the parent to update the respective data/state so that it is updated
+// in all the places where that data is use ( for ex. in case of primitive data )
+// all the data passed to the event is caught by the $event and is passed to the do_this(any_local_name)
+// method. How you use it to update your current state depends on methods.
+
+
+// The event bus
+//
+// The above method of updating the entries relies on updating the value at parent which then results
+// into updating all other places where the data was used.
+// What if we only want to do it for some other childs only, not for parent! We could use the
+// concept of event bus to change the values in needed child directly without involving the parent.
+//
+// How to do it? ex.:
+//
+// Create a new vue instance in the root file and maybe name it bus for better resemblance
+// const bus = new Vue(); // Done
+// export bus;
+//
+// in child component1:
+// import {bus} from '../app' //import the bus instance from app.js
+// ................
+//  methods: { do_something() {
+//     this.value_name = "new_value";
+//     this.$emit("event-name", this.value_name); => pass any data that helps
+//  }}
+//
+// in child component2: import the bus instance and create a lifecycle hook so that an event listener
+// is created as soon as this component is created.
+// import {bus} from '../app'
+// .........................
+// data: {},
+// created() { // a lifecycle hook method, called when the component is created
+//    this.$on(event-name, (data_recieved_from_the_event) => {
+//              ** do something **
+//     })
+// }
+
+
+// Lifecycle hooks
+// We have several lifecycle methods that can be invoked at at particualar, more details on the
+// official vue js site.
+// ex. beforeCreated, created, beforeMounted, mounted, beforeUpdate, updated() etc.
+
+
+// Slots
+// slots are used to insert html elements inside our custom created component tags, we can create 
+// slot with any kind of elements and can give them their own names later they can be sorted and 
+// re arranged on the basis of their names.
+// ex. <form-header><div slot="slot-name"><h1>This is the form title</h1></div></form-header>
+
+
+// Dynamic Components
+// <component is="component-name">
+// We can use the above syntax to display component with a name of component-name
+// To dynamically switch the components being displayed we could bind the is attribute
+// ex. <component v-bind:is="component_to_display">
+// 
+// Down below we can change the values of this.component_to_display to alter the components dynamically
+// A point to note is we need to import the components we will be using.
+//
+// <keep-alive></keep-alive> Can be used to retain the data inside the component around which it is 
+// wrapped. ( the data inside the input fields are retained as they were before in case if the 
+// component is dynamically switched with any other component, thus we don't loose any data)
+
+
+// Input Binding
+// use v-model to bind the data of the input to any variable of your choice
+// ex. <input type="text" v-model="variable_name">
+// we can use v-model.lazy where lazy is a modifier, so we don't update the variable every time a
+// new alphabet is inserted instead when we are done and switch to other field the variable is 
+// updated with the final value.
+
+
+// http request
+// npm install axios
+// use jsonplaceholder for dev purpose get etc/ requests 
